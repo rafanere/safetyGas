ca3873b17ba1883650bab4dae0e9848a29b7378f
Rafae <Rafae@Rafaela>
2020-05-26 22:10:55 -0300

    Criação da classe Usuário e de suas variáveis

---
+++ safetyGas/src/safetygas/Usuario.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package safetygas;
+
+/**
+ *
+ * @author Rafaela
+ */
+public class Usuario {
+    
+    Integer Cod_Usuario;
+    String Email_Usuario;
+    String Nome_Usuario;
+    Integer CPF_Usuario;
+    Integer Identidade_Usuario;
+    Integer CEP_Usuario;
+    Integer Telefone_Usuario;
+    String Endereco_Usuario;
+        
+}
