dd3982ebe5941f49f4ad5ad43629a3d0314ccf6c
Renatobhcity <65986645+Renatobhcity@users.noreply.github.com>
2020-06-04 17:43:14 -0300

    Add files via upload
    Classe contrato

---
+++ safetyGas/src/safetygas/Contrato.java
@@ -0,0 +1,78 @@
+package safetyGas;
+
+import java.util.Date;
+
+public class Contrato {
+	
+	private Integer codContrato;
+	private Date dtaIni;
+	private Date dtaFim;
+	private Float valorContrato;
+	private Boolean status;
+	
+	//Métodos
+	
+	public void ativarContrato () {
+		this.status=true;
+	}
+	public void desativarContrato () {
+		this.status=false;
+	}
+	
+	
+	
+	
+	//Construtor
+	public Contrato () {
+		
+	}
+	
+	public Integer getCodContrato() {
+		return codContrato;
+	}
+	public void setCodContrato(Integer codContrato) {
+		this.codContrato = codContrato;
+	}
+	public Date getDtaIni() {
+		return dtaIni;
+	}
+	public void setDtaIni(Date dtaIni) {
+		this.dtaIni = dtaIni;
+	}
+	public Date getDtaFim() {
+		return dtaFim;
+	}
+	public void setDtaFim(Date dtaFim) {
+		this.dtaFim = dtaFim;
+	}
+	public Float getValorContrato() {
+		return valorContrato;
+	}
+	public void setValorContrato(Float valorContrato) {
+		this.valorContrato = valorContrato;
+	}
+	public Boolean getStatus() {
+		return status;
+	}
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+		
+
+	
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
